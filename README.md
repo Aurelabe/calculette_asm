c53ce3cd70211310245e71935a386ad246098a7b
somebody_else69 <Aurelabe@users.noreply.github.com>
2026-06-26 17:04:10 +0200

    Mise à jour

--- README.md
+++ README.md
@@ -15,3 +15,49 @@ lire trop loin dans le buffer si quelqu'un tape un truc trop long.
 
 ### 1.2. Tests
 <img width="572" height="360" alt="image" src="https://github.com/user-attachments/assets/0f70b2ea-6204-4e49-a878-44bd74720841" />
+
+## 2. v2 : menu + saisie de 2 nombres
+
+On ajoute un menu avec 5 choix (addition, soustraction, multiplication, division, quitter).
+Le programme demande 2 nombres et les affiche (les vrais calculs arrivent aux versions suivantes).
+
+Pour l'instant on gère que les entiers, c'est plus simple. Les flottants viendront plus tard avec les registres XMM et les instructions SSE2.
+
+### 2.1. read_line : lire une ligne caractère par caractère
+
+Je lis la saisie un byte à la fois avec `syscall read` au lieu d'utiliser `fgets` ou autre.
+Pourquoi ? Parce qu'en assembleur on a rien de tout prêt, il faut tout faire soi-même.
+Je stocke chaque caractère dans un buffer jusqu'au `\n` (newline), je remplace le `\n` par `\0` (null terminator), et je retourne la longueur dans `rax`.
+
+https://stackoverflow.com/questions/8194141/how-to-read-a-line-from-stdin-in-assembly
+
+J'utilise R8 comme compteur de boucle. Pourquoi pas RCX ? Parce que `syscall` écrase RCX (et R11). Si j'utilisais RCX, la valeur serait perdue à chaque appel système.
+
+### 2.2. atoi (lire_entier)
+
+Je convertis la chaîne en entier : je boucle sur chaque caractère, je soustrais '0', et je construis le nombre avec `imul rax, 10` + `add`. Si le premier caractère est '-', je mets un flag et à la fin je fais `neg rax`.
+
+https://stackoverflow.com/questions/19309749/nasm-assembly-convert-input-to-integer
+
+Gestion d'erreur : si un caractère n'est pas un chiffre, ça affiche "Saisie invalide." et on recommence.
+
+### 2.3. itoa (afficher_entier)
+
+Je divise le nombre par 10 en boucle, je récupère le reste (chiffre) avec `div rbx`, j'empile les chiffres, puis je dépile pour écrire dans le buffer. Comme ça les chiffres sont dans le bon ordre.
+
+https://stackoverflow.com/questions/13166064/how-do-i-print-an-integer-in-assembly-level-programming-without-printf-from-the
+
+Pour les nombres négatifs, j'écris un '-' au début et je rends le nombre positif avant la boucle.
+
+### 2.4. Pourquoi default rel ?
+
+NASM affichait un warning : "indirect address displacements cannot be RIP-relative" sur les lignes avec `lea rsi, [rel buf + r8]`. En mettant `default rel` en haut du fichier et en écrivant `lea rsi, [r8 + buf]`, le warning disparaît.
+
+### 2.5. Pourquoi R8 et pas RCX dans read_line ?
+
+`syscall` clobbe RCX et R11 (il les utilise pour sauvegarder RIP et RFLAGS). Donc si on se sert de RCX comme compteur, sa valeur est perdue à chaque `syscall`. R8, R9, R10, R12-R15 ne sont pas touchés par `syscall`.
+
+https://stackoverflow.com/questions/55691159/why-argc-is-stored-in-stack-but-not-rdi-register-in-x86-64-on-linux
+
+### 2.6. Tests
+<!-- screenshot placeholder v2 -->
--- calc.asm
+++ calc.asm
@@ -1,95 +1,281 @@
-; calc.asm - Calculatrice confidentielle (v1: mot de passe)
+; calc.asm - Calculatrice (v2: mot de passe + menu + 2 nombres)
 ; nasm -f elf64 calc.asm -o calc.o && ld calc.o -o calc
 default rel
+
 section .data
-    prompt   db 'Code : '
-    prompt_l equ $ - prompt
-    ok_msg   db '=== Calculatrice ===', 10
-    ok_l     equ $ - ok_msg
-    fail1    db 'Code incorrect. Plus que '
-    fail1_l  equ $ - fail1
-    fail2    db ' tentatives.', 10
-    fail2_l  equ $ - fail2
-    block    db 'Programme verrouille.', 10
-    block_l  equ $ - block
+    ; --- messages du mot de passe ---
+    prompt   db 'Entrez le mot de passe : ', 0
+    ok_msg   db '=== Calculatrice ===', 10, 0
+    fail1    db 'Mot de passe incorrect. Plus que ', 0
+    fail2    db ' tentatives.', 10, 0
+    block    db 'Programme verrouillé.', 10, 0
+    ; --- menu ---
+    menu_txt db 10, 'Menu :', 10
+             db '  1. Addition (+)', 10
+             db '  2. Soustraction (-)', 10
+             db '  3. Multiplication (*)', 10
+             db '  4. Division (/)', 10
+             db '  5. Quitter', 10
+             db 'Choix : ', 0
+    prompt_a db 'Premier nombre : ', 0
+    prompt_b db 'Deuxieme nombre : ', 0
+    res_txt  db 'Resultat : ', 0
+    err_inv  db 'Saisie invalide.', 10, 0
+    virgule  db ', ', 0
     secret   db 'asm42'             ; mot de passe secret
+    secret_len equ 5                ; longueur secret
+    max_att  equ 3                  ; tentatives max
+
 section .bss
-    input    resb 64                ; buffer pour la saisie
+    buf      resb 64                ; buffer saisie + affichage
+    choix    resb 1                 ; choix menu
+    nb1      resq 1                 ; 1er nombre
+    nb2      resq 1                 ; 2e nombre
+
 section .text
     global _start
 
 _start:
-    mov rcx, 3                      ; 3 tentatives max
-.debut:
+    call check_pass
+.boucle:
+    call afficher_menu
+    cmp byte [choix], '5'
+    je fin
+    call lire_deux_nombres
+    jmp .boucle
+
+check_pass:
+    mov rcx, max_att
+.l:
     push rcx
-    ; --- afficher le prompt et lire la saisie ---
-    mov rax, 1
-    mov rdi, 1
     mov rsi, prompt
-    mov rdx, prompt_l
-    syscall
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, input
-    mov rdx, 64
-    syscall
-    dec rax
-    mov rdx, rax
-    cmp rdx, 5
-    jne .erreur
-    ; --- verifier le mot de passe ---
-    mov rsi, secret
-    mov rdi, input
-    mov rcx, 5
-.comparer:
+    call print
+    call read_line
+    cmp rax, secret_len
+    jne .e
+    mov rsi, buf
+    mov rdi, secret
+    mov rcx, secret_len
+.c:
     mov al, [rsi]
     mov bl, [rdi]
     cmp al, bl
-    jne .erreur
+    jne .e
     inc rsi
     inc rdi
     dec rcx
-    jnz .comparer
+    jnz .c
     pop rcx
-    mov rax, 1
-    mov rdi, 1
     mov rsi, ok_msg
-    mov rdx, ok_l
-    syscall
-    jmp fin
-.erreur:
+    call print
+    ret
+.e:
     pop rcx
     dec rcx
-    jz .bloque
+    jz .b
     push rcx
-    mov rax, 1
-    mov rdi, 1
     mov rsi, fail1
-    mov rdx, fail1_l
-    syscall
+    call print
     pop rcx
     push rcx
     add rcx, '0'
-    mov [input], cl
-    mov byte [input+1], 0
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, input
+    mov [buf], cl
+    mov byte [buf+1], 0
+    mov rsi, buf
+    call print
+    mov rsi, fail2
+    call print
+    pop rcx
+    jmp .l
+.b:
+    mov rsi, block
+    call print
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+afficher_menu:
+    mov rsi, menu_txt
+    call print
+.l:
+    call read_line
+    cmp rax, 0
+    je fin
+    cmp rax, 1
+    jne .inv
+    mov al, [buf]
+    cmp al, '1'
+    jb .inv
+    cmp al, '5'
+    ja .inv
+    mov [choix], al
+    ret
+.inv:
+    mov rsi, err_inv
+    call print
+    jmp .l
+
+lire_deux_nombres:
+    mov rsi, prompt_a
+    call print
+    call lire_entier
+    mov [nb1], rax
+    mov rsi, prompt_b
+    call print
+    call lire_entier
+    mov [nb2], rax
+    mov rsi, res_txt
+    call print
+    mov rax, [nb1]
+    call afficher_entier
+    mov rsi, virgule
+    call print
+    mov rax, [nb2]
+    call afficher_entier
+    mov rsi, newline
+    call print
+    ret
+
+lire_entier:
+    push rbx
+    push rcx
+    call read_line
+    cmp rax, 0
+    je .eof
+    mov rsi, buf
+    xor rax, rax
+    xor rcx, rcx
+    cmp byte [rsi], '-'
+    jne .p
+    mov rcx, 1
+    inc rsi
+.p:
+    movzx rdx, byte [rsi]
+    cmp rdx, 0
+    je .fin
+    cmp rdx, '0'
+    jb .inv
+    cmp rdx, '9'
+    ja .inv
+    sub rdx, '0'
+    imul rax, rax, 10
+    add rax, rdx
+    inc rsi
+    jmp .p
+.fin:
+    cmp rcx, 0
+    je .ok
+    neg rax
+.ok:
+    pop rcx
+    pop rbx
+    ret
+.inv:
+    mov rsi, err_inv
+    call print
+    pop rcx
+    pop rbx
+    jmp lire_entier
+.eof:
+    pop rcx
+    pop rbx
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+afficher_entier:
+    push rbx
+    push rcx
+    push rdx
+    mov rdi, buf
+    xor rcx, rcx
+    cmp rax, 0
+    jge .pos
+    mov byte [rdi], '-'
+    inc rdi
+    inc rcx
+    neg rax
+.pos:
+    mov rbx, 10
+    push 0
+.l:
+    xor rdx, rdx
+    div rbx
+    add dl, '0'
+    push rdx
+    cmp rax, 0
+    jne .l
+.w:
+    pop rax
+    cmp rax, 0
+    je .fin
+    mov [rdi], al
+    inc rdi
+    inc rcx
+    jmp .w
+.fin:
+    mov byte [rdi], 0
+    mov rsi, buf
+    call print
+    mov rax, rcx
+    pop rdx
+    pop rcx
+    pop rbx
+    ret
+
+read_line:
+    push r8
+    xor r8, r8
+.l:
+    mov rax, 0
+    mov rdi, 0
+    lea rsi, [r8 + buf]
     mov rdx, 1
     syscall
+    cmp rax, 1
+    jne .eof
+    cmp byte [buf + r8], 10
+    je .fin
+    inc r8
+    cmp r8, 62
+    jb .l
+.fin:
+    mov byte [buf + r8], 0
+    mov rax, r8
+    pop r8
+    ret
+.eof:
+    mov byte [buf + r8], 0
+    mov rax, r8
+    pop r8
+    ret
+
+newline db 10, 0
+
+print:
+    push rdi
+    push rdx
+    push rsi
+    call strlen
+    mov rdx, rax
+    pop rsi
     mov rax, 1
     mov rdi, 1
-    mov rsi, fail2
-    mov rdx, fail2_l
     syscall
-    pop rcx
-    jmp .debut
-.bloque:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, block
-    mov rdx, block_l
-    syscall
+    pop rdx
+    pop rdi
+    ret
+
+strlen:
+    xor rax, rax
+.l:
+    cmp byte [rsi + rax], 0
+    je .fin
+    inc rax
+    jmp .l
+.fin:
+    ret
+
 fin:
     mov rax, 60
     xor rdi, rdi
